00a7ddf7f2a98a303c0d898753e583f4f60b0463
MJ Bembe <mncedisie@gmail.com>
2020-04-02 09:37:02 +0200

    Second Commit

---
+++ Users/Mncedisi.Bembe/Documents/NetBeansProjects/ADDITION/src/addition/ADDITION.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addition;
+
+/**
+ *
+ * @author Mncedisi.Bembe
+ */
+
+
+public class ADDITION{
+    public static void main(String[] args){
+    
+    int x=2;
+    int y=4;
+    int z= x+y;
+    System.out.println("The sum of "+x+" and " +y+ " is "+z);
+    }
+
+}
